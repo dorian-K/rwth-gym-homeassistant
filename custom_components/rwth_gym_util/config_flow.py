3d03d1f3258a2038858b566851e1714c28b09266
dork <62394594+dorian-K@users.noreply.github.com>
2023-07-20 15:14:52 +0200

    Add back config flow

---
+++ custom_components/rwth_gym_util/config_flow.py
@@ -0,0 +1,12 @@
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_entry_flow
+
+from .const import DOMAIN
+
+
+async def _async_has_devices(hass: HomeAssistant) -> bool:
+    return True
+
+
+config_entry_flow.register_discovery_flow(
+    DOMAIN, "NEW_NAME", _async_has_devices)
